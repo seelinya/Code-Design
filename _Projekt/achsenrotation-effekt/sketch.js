4e96ced62b2f8cbfa6e619c1e57d1ef5877b507d
Selina Strickler <strickler.selina@gmail.com>
2020-09-30 07:24:51 +0200

    windrad

--- _Projekt/achsenrotation-effekt/sketch.js
+++ _Projekt/achsenrotation-effekt/sketch.js
@@ -1,20 +1,25 @@
+let angle = 0;
+
 function setup() {
-  createCanvas(1800, 1000);
+  createCanvas(600, 400);
+  angleMode(DEGREES);
   //noStroke();
   //noLoop();
 }
 
 function draw() {
   background(0);
-  strokeWeight(1);
+  //strokeWeight(4);
+  //stroke(255);
+  //noFill();
+  translate(200, 200);
+  rotate(angle);
   stroke(255);
+  line(0, 0, 50, 50);
+  //line(0, 25, 100, 100);
+  fill(255);
+  rect(50, 50, 100, 50);
 
-  for (var x = 20; x <= 1800; x += 50) {
-    for (var y = 20; y <= 1000; y += 50) {
-    fill(random(255), 0, random(255));
-    rotate(PI / random(90));
-    rect(x, y, 10, 10)
-    }
-  }
+  angle = angle + 1;
 
 }
