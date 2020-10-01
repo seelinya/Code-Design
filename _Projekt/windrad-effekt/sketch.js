76473c75aad6f7e52bd0259e85df632366f966ad
Selina Strickler <strickler.selina@gmail.com>
2020-10-01 07:19:13 +0200

    nice try

--- _Projekt/windrad-effekt/sketch.js
+++ _Projekt/windrad-effekt/sketch.js
@@ -12,22 +12,32 @@ function draw() {
   background(0);
   strokeWeight(1);
   stroke(255);
-  translate(400, 400);
+  translate(450, 450);
   rotate(angle);
 
   strokeWeight(1);
   stroke(255);
-  line(20, 60, 500, 60);
+  //line(0, 0, 600, 90);
 
-  for (var x = 20; x <= 300; x = x + 20) {
-    for (var y = 20; y <= 80; y = y + 20) {
+  //rotate(PI / 10);
+  translate(180, -60);
+  for (var x = 30; x <= 450; x = x + 30) {
+    for (var y = 30; y <= 120; y = y + 30) {
     fill(255, 0);
     //fill(random(255), 0, random(255, 0)); //kann eigentlich ausgeblendet werden
     //ellipse(x, y, 10, 10) //kann eigentlich ausgeblendet werden
     rect(x, y, 20, 20)
+    //line(0, 0, 600, 90);
     }
   }
-  angle = angle + 1; //die letzte Zahl kann die Geschwindigkeit steuern
+
+    for (var x = 30; x <= 450; x = x + 30) {
+      for (var y = 30; y <= 120; y = y + 30) {
+      fill(255, 0);
+      rect(x, y, 20, 20)
+      }
+  }
+  angle = angle + 0.3; //die letzte Zahl kann die Geschwindigkeit steuern
 
 
 }
