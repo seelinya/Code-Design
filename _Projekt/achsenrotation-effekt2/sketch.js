bf41b6c01e135e09f8297ca7a67de0985a2991fd
hzuellig <zuellig@internauta.ch>
2020-10-15 18:14:00 +0200

    before pull

--- _Projekt/achsenrotation-effekt2/sketch.js
+++ _Projekt/achsenrotation-effekt2/sketch.js
@@ -10,7 +10,7 @@ function draw() {
   strokeWeight(1);
   stroke(255);
   //rectMode(CENTER);
-
+randomSeed(5);
   for (var x = 20; x <= 1800; x += 50) {
     for (var y = 20; y <= 1000; y += 50) {
     fill(random(255), 0, random(255));
